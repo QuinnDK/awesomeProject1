570b053a7e2c998a11fa643ed77cb493642febfe
QuinnDK <18207205276@163.com>
2020-04-14 10:07:14 +0800

    第一次提交

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
 	"fmt"
 )
 
-func main()  {
+func main() {
+	//day1.1.go
 	var a string = "Dongkun"
 	fmt.Println(a)
 
 	var c, b int = 1, 3
-	fmt.Println(c,b)
+	fmt.Println(c, b)
 
 	var _, number, str = text.Numbers()
-	fmt.Println(number,str)
+	fmt.Println(number, str)
 
+	//day1.2.go
 	text.Text()
+
+	//day1.3.go使用
+	text.FindSingle()
 
 }
