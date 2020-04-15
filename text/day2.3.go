9b830e6fc7c1a922e292c437897fd302daa472cb
QuinnDK <18207205276@163.com>
2020-04-15 17:08:20 +0800

    第二次提交，最近要忙入党事宜，以及关于DAG论文，头大啊

--- text/day2.3.go
+++ text/day2.3.go
@@ -18,6 +18,7 @@ func Add(x1, x2 int) func() (int, int) {
 
 //第一个函数是用来传入参数，func 是使用参数 ，（int,int,int）是Add函数返回的参数类型
 //说起类型，数组指针，指针数组，函数指针要加紧看了，快忘光了
+
 func Add1(x1, x2 int) func(x3 int, x4 int) (int, int, int) {
 	i := 0
 	return func(x3 int, x4 int) (int, int, int) {
